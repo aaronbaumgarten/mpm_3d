eab93cc32034b8630518c83b100ac0a47cf8d3e8
Aaron S Baumgarten <asbaum@mit.edu>
2017-01-30 13:02:04 -0500

    Fixed material models. Added ability to run from directory other than build.

---
+++ mpm_pygen/GenerateProjectile.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+import sys
+import math
+
+import Primitives3d
+import CSGTree3d
+import Grid3d
+
+if (len(sys.argv) > 1):
+    particle_filename = sys.argv[-1] + '.particles'
+    grid_filename = sys.argv[-1] + '.grid'
+else:
+    print sys.argv[0], "outprefix"
+    exit(0)
+print "files named"
+
+#grid properties
+#Ly = Lx = Lz = 0.4
+Lx = 0.1
+Ly = 0.2
+Lz = 0.005
+#Ne = 40
+Nx = 50
+Ny = 100
+Nz = 1
+lmpp = 2
+grid = Grid3d.CartesianPointGrid(Lx, Ly, Lz, Nx, Ny, Nz, lmpp)
+print "grid created"
+
+# global properties
+g = -9.81
+
+# free block properties
+bulk_properties = { 'rho': 1500.0 }
+grain_width = Lx
+grain_depth = 0.5*Ly
+grain_height = 0.5*Lz
+
+bulk_primitive = Primitives3d.Box(0,grain_width,
+                                 0,grain_depth,
+                                 0,grain_height,
+                                 )
+bulk_body = CSGTree3d.Node(bulk_primitive)
+print "bulk created"
+
+# add points to arrays
+bulk_point_array = []
+grid.point_array = []
+grid.generate_point_array(bulk_primitive)
+bulk_point_array = grid.point_array
+
+
+# free block properties
+block_properties = { 'rho': 2700.0 }
+block_width = 0.02#*Lx
+block_height = 0.5*Lz
+block_depth = 0.02#*Ly
+
+block_primitive = Primitives3d.Cylinder(Primitives3d.Point(Lx/2,3*Ly/4,0),
+                                        Primitives3d.Point(Lx/2,3*Ly/4,block_height),
+                                        block_width/2)
+                  #Primitives3d.Box(#(Lx-block_width)/2, (Lx+block_width)/2,
+                  #               block_width,block_width+block_width,
+                  #               (Ly-block_depth)/2, (Ly+block_depth)/2,
+                  #               #0,block_height,
+                  #               (Lz-block_height)/2, (Lz+block_height)/2,
+                  #               )
+block_body = CSGTree3d.Node(block_primitive)
+print "block created"
+
+# add points to arrays
+block_point_array = []
+grid.generate_point_array(block_primitive)
+block_point_array = grid.point_array
+
+
+# add arrays to mpm dictionary
+mpm_points = []
+nb1 = len(bulk_point_array) #number of material points in body 1
+nb2 = len(block_point_array)
+
+grid.write_grid_file(grid_filename)
+with open(particle_filename, 'w') as f:
+    f.write("%d\n" % 2)
+    f.write("%d\n" % nb1)
+    f.write("%d\n" % nb2)
+    pID = 0
+    for p in bulk_point_array:
+    #    mpm_point = {
+    #                    'body': 1,
+    #                    'm': bulk_properties['rho']*grid.material_point_volume,
+    #                    'v': grid.material_point_volume,
+    #                    'x': p.x, 'y': p.y, 'z': p.z,
+    #                    'x_t':0, 'y_t':0, 'z_t':0 #,
+    #                    #'sxx': 0, 'sxy': 0, 'syy': 0
+    #                }
+    #    if (mpm_point['body'] == 1):
+    #        nb1 += 1
+    #    mpm_points.append(mpm_point)
+        f.write("%g %g %g %g %g %g %g %g %g %g\n" % (0, pID, bulk_properties['rho']*grid.material_point_volume, grid.material_point_volume, p.x, p.y, p.z, 0, 0, 0))
+        pID += 1
+    pID = 0
+    for p in block_point_array:
+        f.write("%g %g %g %g %g %g %g %g %g %g\n" % (1, pID, block_properties['rho']*grid.material_point_volume, grid.material_point_volume, p.x, p.y, p.z, 0, -100, 0))
+        pID += 1
+
+print "file written"
---
+++ src/materials/isolin_wheel.cpp
@@ -0,0 +1,173 @@
+//
+// Created by aaron on 1/26/17.
+// isolin_wheel.cpp.cpp
+//
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <vector>
+#include <math.h>
+#include "particle.hpp"
+#include "node.hpp"
+#include "body.hpp"
+#include "process.hpp"
+#include "tensor.hpp"
+
+#define MAT_VERSION_STRING "1.0" __DATE__ " " __TIME__
+
+/* timestep passed as argument */
+double dt = 1e-6;
+
+/* Material Constants (set by configuration file). */
+double E, nu, G, K;
+double lambda;
+
+extern "C" void material_init(Body *body);
+
+extern "C" void calculate_stress_threaded(threadtask_t *task, Body *body, double dt);
+
+extern "C" void calculate_stress(Body *body, double dt);
+
+extern "C" void calculate_stress_implicit(Body *body, double dt);
+
+#define SZZ_STATE 0
+
+/*----------------------------------------------------------------------------*/
+void material_init(Body *body) {
+    size_t i, j;
+
+    for (i = 0; i < body->p; i++) {
+        for (j = 0; j < DEPVAR; j++) {
+            body->particles.state(i,j) = 0;
+        }
+    }
+
+    if (body->material.num_fp64_props < 2) {
+        std::cout << body->material.num_fp64_props << "\n";
+        fprintf(stderr,
+                "%s:%s: Need at least 2 properties defined (E, nu).\n",
+                __FILE__, __func__);
+        exit(0); //replace error code handler from sachith's work with 0
+    } else {
+        E = body->material.fp64_props[0];
+        nu = body->material.fp64_props[1];
+        G = E / (2.0 * (1.0 + nu));
+        K = E / (3.0 * (1.0 - 2 * nu));
+        lambda = K - 2.0 * G / 3.0;
+        printf("Material properties (E = %g, nu = %g, G = %g, K = %g).\n",
+               E, nu, G, K);
+    }
+
+    std::cout << "Done initializing material (" << body->id << ").\n";
+    return;
+}
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------*/
+void calculate_stress(Body *body, double dtIn) {
+    dt = dtIn;
+    threadtask_t t;
+    t.offset = 0;
+    t.blocksize = body->p;
+    calculate_stress_threaded(&t,body, dtIn);
+    return;
+}
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------*/
+void calculate_stress_implicit(Body *body, double dtIn) {
+
+    dt = dtIn;
+
+    for (size_t i = 0; i < body->p; i++) {
+        if (body->particles.active[i] == 0) {
+            continue;
+        }
+
+        Eigen::VectorXd tmpVec(9);
+
+        tmpVec << body->particles.L.row(i).transpose();
+        Eigen::Matrix3d L(tmpVec.data());
+
+        tmpVec << body->particles.T.row(i).transpose();
+        Eigen::Matrix3d T(tmpVec.data());
+
+        Eigen::Matrix3d D = 0.5*(L+L.transpose());
+        Eigen::Matrix3d W = 0.5*(L-L.transpose());
+
+        double trD = D.trace();
+
+        Eigen::Matrix3d gleft = W*T;
+        Eigen::Matrix3d gright = T*W;
+
+        Eigen::Matrix3d tmpMat = gleft-gright;
+
+        Eigen::Matrix3d CD = 2*G*D + lambda*trD*Eigen::Matrix3d::Identity();
+
+        Eigen::Matrix3d dsj = CD + tmpMat;
+
+        for (size_t pos=0;pos<9;pos++){
+            body->particles.Ttrial(i,pos) = body->particles.T(i,pos) + dt*dsj(pos);
+        }
+    }
+
+}
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------*/
+void calculate_stress_threaded(threadtask_t *task, Body *body, double dtIn) {
+    //printf("t = %g\nx = %g\ny = %g\n", job->t, job->particles[0].x, job->particles[0].y);
+    /*printf("L: \n%g %g %g\n%g %g %g\n%g %g %g\n\n",
+           job->particles[0].L[0],
+           job->particles[0].L[1],
+           job->particles[0].L[2],
+           job->particles[0].L[3],
+           job->particles[0].L[4],
+           job->particles[0].L[5],
+           job->particles[0].L[6],
+           job->particles[0].L[7],
+           job->particles[0].L[8]);*/
+    /* Since this is local, we can split the particles among the threads. */
+    dt = dtIn;
+    size_t p_start = task->offset;
+    size_t p_stop = task->offset + task->blocksize;
+
+    for (size_t i = 0; i < body->p; i++) {
+        if (body->particles.active[i] == 0) {
+            continue;
+        }
+
+        Eigen::VectorXd tmpVec(9);
+
+        tmpVec << body->particles.L.row(i).transpose();
+        Eigen::Matrix3d L(tmpVec.data());
+
+        tmpVec << body->particles.T.row(i).transpose();
+        Eigen::Matrix3d T(tmpVec.data());
+
+        Eigen::Matrix3d D = 0.5*(L+L.transpose());
+        Eigen::Matrix3d W = 0.5*(L-L.transpose());
+
+        double trD = D.trace();
+
+        Eigen::Matrix3d gleft = W*T;
+        Eigen::Matrix3d gright = T*W;
+
+        Eigen::Matrix3d tmp = gleft-gright;
+
+        Eigen::Matrix3d CD = 2*G*D + lambda*trD*Eigen::Matrix3d::Identity();
+
+        Eigen::Matrix3d dsj = CD + tmp;
+
+        for (size_t pos=0;pos<9;pos++){
+            body->particles.T(i,pos) += dt*dsj(pos);
+        }
+    }
+
+    return;
+}
+/*----------------------------------------------------------------------------*/
+
+
